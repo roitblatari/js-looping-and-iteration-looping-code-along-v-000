5c4697bd5c93880767c601843d69ab984d96ca98
Aron Roitblat <roitblatari@gmail.com>
2018-09-14 00:50:01 -0400

    Update index.js
    submit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+// const arr = [];
+function printBadges(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    console.log( `Welcome ${arr[i]}! You are employee #${ i + 1 }.`);
+  }
+  return arr;
+}
+
+function tailsNeverFails() {
+  let tails = 0;
+  while (Math.random() >= 0.5) {
+    ++tails;
+  }
+  return `You got ${tails} tails in a row!`;
+}
